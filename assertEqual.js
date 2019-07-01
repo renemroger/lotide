f291401197325d2fdbb0c84016e90aba004bc94b
mroger <rene.mroger@gmail.com>
2019-07-01 20:31:10 +0000

    adding test Folder and creating head Test

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅  Assertion Passed:  ${actual}  ===  ${expected}`);
   } else {
@@ -7,6 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = { assertEqual };
